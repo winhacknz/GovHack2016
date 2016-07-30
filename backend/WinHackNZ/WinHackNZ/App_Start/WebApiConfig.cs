841f25eeabd6a73b0961c155ba057d000eebc41e
YiniYin <yiniski@gmail.com>
2016-07-30 12:14:14 +1200

    add helper class to make rest call

--- backend/WinHackNZ/WinHackNZ/App_Start/WebApiConfig.cs
+++ backend/WinHackNZ/WinHackNZ/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
@@ -25,6 +26,9 @@ namespace WinHackNZ
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
---
+++ backend/WinHackNZ/WinHackNZ/Controllers/SafetyController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WinHackNZ.Helper;
+
+namespace WinHackNZ.Controllers
+{
+    [RoutePrefix("api/Safety")]
+    public class SafetyController : ApiController
+    {
+        // GET api/
+        public string Get()
+        {
+            var endPoint = @"http:\\myRestService.com\api\";
+
+            var client = new RestClient(endPoint);
+            var json = client.MakeRequest();
+
+            return json;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
