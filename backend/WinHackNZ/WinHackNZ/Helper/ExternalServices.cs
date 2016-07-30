d8848a89d86ea157d94178a7f5320ebca0153fbd
Matthew <climbandrave@gmail.com>
2016-07-30 20:23:22 +1200

    Just added additional IBM api

--- backend/WinHackNZ/WinHackNZ/Helper/ExternalServices.cs
+++ backend/WinHackNZ/WinHackNZ/Helper/ExternalServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,7 +10,7 @@ namespace WinHackNZ.Helper
 {
     public class ExternalServices
     {
-        public static string GetRentalData()
+        public static JObject GetRentalData()
         {
             var endPoint    = @"https://api.tmsandbox.co.nz/v1/Search/Property/Rental.json";
             var oAuthHeader = @"Authorization: OAuth oauth_consumer_key=32038210D256885F3FC11DF0A42DBA89, oauth_signature_method=PLAINTEXT, oauth_signature=89F5A44725D07B153C1163BC1149E932&";
@@ -20,7 +21,16 @@ namespace WinHackNZ.Helper
             
             var json = client.MakeRequest();
 
-            return json;
+            return JObject.Parse(json);
+        }
+
+        public static JObject GetBadCityNew(string cityName)
+        {
+            var endPoint = @"https://access.alchemyapi.com/calls/data/GetNews?apikey=f29c6c4716f07c183dbb75aeb46fae77f98860d5&return=enriched.url.title&start=1469232000&end=1469919600&q.enriched.url.enrichedTitle.entities.entity=|text=" + cityName + @",type=city|&q.enriched.url.enrichedTitle.docSentiment.type=$sentiment&count=25&outputMode=json";
+            var client = new RestClient(endPoint);
+            var json = client.MakeRequest();
+
+            return JObject.Parse(json);
         }
     }
 }
--- backend/WinHackNZ/WinHackNZTest/UnitTest1.cs
+++ backend/WinHackNZ/WinHackNZTest/UnitTest1.cs
@@ -8,9 +8,16 @@ namespace WinHackNZTest
     public class ExternalServices_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Get_Rental()
         {
             var x = ExternalServices.GetRentalData();
+            Assert.IsNotNull(x);
+        }
+
+        [TestMethod]
+        public void Test_Get_City_News()
+        {
+            var x = ExternalServices.GetBadCityNew("Auckland");
             Assert.IsNotNull(x);
         }
     }
